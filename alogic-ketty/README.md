3f0e0833dc9a6b41a35b2b7c6d487c512cc5be50
duanyy <hmyyduan@gmail.com>
2014-10-29 10:10:02 +0800

    alogic-addons-1.3.0.1
    采用alogic-remote组件来实现metrics对外输出插件(1.3.0.1)

---
+++ alogic-ketty/README.md
@@ -0,0 +1,16 @@
+alogic-addons readme
+====================
+
+### 概述
+alogic-addons是alogic附加的定制包，对alogic-core各项功能进行了具体化的定制，包括：
+
+- com.logicbus.backend.acm,对alogic的访问控制模型进行定制,提供了ACM模型插件支持；
+
+- com.logicbus.backend.metrics,对指标的处理进行了定制，提供了对外输出的插件
+
+- com.logicbus.service，提供了dbcp、together、remote等模块的管理服务
+
+### 版本
+ - 1.3.0 [20141023 duanyy]
+ 	 + 初次发布，由anyLogicBus、anyFormula和anyWebLoader合并而来
+ 	 + 采用alogic-remote组件来实现metrics对外输出插件(1.3.0.1)
---
+++ alogic-ketty/src/main/java/com/logicbus/backend/metrics/handler/RemoteWriter.java
@@ -0,0 +1,91 @@
+package com.logicbus.backend.metrics.handler;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.w3c.dom.Element;
+
+import com.anysoft.context.Context;
+import com.anysoft.metrics.core.Dimensions;
+import com.anysoft.metrics.core.Fragment;
+import com.anysoft.metrics.handler.SummaryWriter;
+import com.anysoft.util.Properties;
+import com.anysoft.util.PropertiesConstants;
+import com.anysoft.util.Settings;
+import com.logicbus.remote.context.CallSource;
+import com.logicbus.remote.context.InnerContext;
+import com.logicbus.remote.core.BuilderFactory;
+import com.logicbus.remote.core.Call;
+import com.logicbus.remote.core.Parameters;
+
+
+/**
+ * 通过远程调用输出指标数据
+ * 
+ * @author duanyy
+ *
+ */
+public class RemoteWriter extends SummaryWriter {
+
+	
+	protected void write(Map<String, Fragment> _data,long t) {
+		if (theCall != null){
+			
+			try {
+				Parameters paras = theCall.createParameter();
+	
+				{				
+					List<Object> data = new ArrayList<Object>();
+					Collection<Fragment> values = _data.values();
+				
+					Settings settings = Settings.get();
+					if (host == null){
+						host = settings.GetValue("host", "${server.host}:${server.port}");
+					}
+					if (app == null){
+						app = settings.GetValue("app", "${server.app}");
+					}
+					for (Fragment item:values){
+						Map<String,Object> map = new HashMap<String,Object>(5);
+						
+						Dimensions dims = item.getDimensions();
+						
+						if (dims != null){
+							dims.lpush(host,app);
+						}
+						
+						item.toJson(map);
+						data.add(map);
+					}
+					
+					paras.param("data", data,BuilderFactory.LIST);
+				}
+				
+				theCall.execute(paras);
+			}catch (Exception ex){
+				
+			}
+		}
+	}
+
+	
+	protected void onConfigure(Element e, Properties p) {
+		String callId = PropertiesConstants.getString(p, "callId", "metricsOutput");
+
+		Context<Call> context = new InnerContext();			
+		context.configure(e, p);
+		theCall = context.get(callId);
+		if (theCall == null){
+			theCall = CallSource.getCall(callId);
+		}
+	}
+	
+	protected Call theCall = null;
+	
+	protected String host;
+	protected String app;
+}
