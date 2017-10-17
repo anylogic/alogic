e294c6c68efa8828719966ae5c17ec980b0c6e11
段云涌 <szduanyy@189.cn>
2017-10-17 14:39:19 +0800

    alogic-1.6.10.4
    - alogic-kvalue:RedisPool优化密码取值功能;

--- alogic-kvalue/src/main/java/com/logicbus/redis/context/RedisPool.java
+++ alogic-kvalue/src/main/java/com/logicbus/redis/context/RedisPool.java
@@ -2,13 +2,18 @@ package com.logicbus.redis.context;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Element;
 
 import com.alogic.pool.impl.Queued;
 import com.anysoft.util.BaseException;
+import com.anysoft.util.Confirmer;
 import com.anysoft.util.Properties;
 import com.anysoft.util.PropertiesConstants;
+import com.anysoft.util.Settings;
 import com.anysoft.util.XmlElementProperties;
+import com.anysoft.util.code.Coder;
+import com.anysoft.util.code.CoderFactory;
 import com.logicbus.redis.client.Client;
 import com.logicbus.redis.client.Protocol;
 import com.logicbus.redis.util.RedisContextException;
@@ -20,6 +25,9 @@ import com.logicbus.redis.util.RedisException;
  * @author duanyy
  * @version 1.6.6.9 [20161209 duanyy] <br>
  * - 从新的框架下继承 <br>
+ * 
+ * @version 1.6.10.4 [20171017 duanyy] <br>
+ * - 优化密码取值功能 <br>
  */
 public class RedisPool extends Queued{
 
@@ -27,31 +35,65 @@ public class RedisPool extends Queued{
 	 * host
 	 */
 	protected String host;
-	
-	public String getHost(){return host;};
-	
+
 	/**
 	 * port
 	 */
 	protected int port = Protocol.DEFAULT_PORT;
-	public int getPort(){return port;}
 	
+	/**
+	 * id
+	 */
 	protected String id;
-	public String getId(){return id;}
 	
 	/**
 	 * password
 	 */
 	protected String password = "";
 	
+	/**
+	 * 缺省用户名(redis不需要用户名验证，在这里只是作为密码加密的key)
+	 */
+	protected String username = "alogic";
+	
+	/**
+	 * 缺省的db
+	 */
 	protected int db = 0;
-	protected int getDB(){return db;}
 	
 	/**
 	 * time out to get client
 	 */
 	protected int timeout = 30000;
-		
+	
+	/**
+	 * 数据确认类的类名
+	 */
+	protected String callback = "";
+	
+	/**
+	 * 数据确认ID
+	 */
+	protected String callbackId = "";
+	
+	/**
+	 * 数据确认者
+	 */
+	protected Confirmer confirmer = null;
+	
+	/**
+	 * 加密的coder
+	 */
+	protected String coder = "Default";	
+	
+	public String getHost(){return host;}
+	
+	public int getPort(){return port;}
+	
+	protected int getDB(){return db;}
+	
+	public String getId(){return id;}
+	
 	protected String getIdOfMaxQueueLength() {
 		return "maxActive";
 	}
@@ -81,10 +123,45 @@ public class RedisPool extends Queued{
 		returnObject(client,false);
 	}
 	
+	@SuppressWarnings("unchecked")
 	protected <pooled> pooled createObject(){
-		Client instance =  new Client(host,port,password,db);
-		instance.register(this);
-		return (pooled) instance;
+		Client instance = null;
+		try {
+			ClassLoader cl = Settings.getClassLoader();
+			if (confirmer == null){
+				if (StringUtils.isNotEmpty(callbackId) 
+						&& StringUtils.isNotEmpty(callback)){
+					try {
+						confirmer = (Confirmer)cl.loadClass(callback).newInstance();
+						confirmer.prepare(callbackId);
+					}catch (Exception ex){
+						
+					}
+				}
+			}	
+			
+			if (confirmer == null){
+				String pwd = password;
+				if (StringUtils.isNotEmpty(coder)){
+					//通过coder进行密码解密
+					try {
+						Coder _coder = CoderFactory.newCoder(coder);
+						pwd = _coder.decode(password, username);
+					}catch (Exception ex){
+						logger.error("Can not find coder:" + coder);
+					}
+				}
+				instance =  new Client(host,port,pwd,db);
+				instance.register(this);
+			}else{
+				String pwd = confirmer.confirm("password", password);
+				instance =  new Client(host,port,pwd,db);
+				instance.register(this);
+			}
+		}catch (Exception ex){
+			logger.error(String.format("Can not create a connection to redis %d:%d",host,port),ex);
+		}
+		return (pooled)instance;
 	}
 
 	public void configure(Element _e, Properties _properties)
@@ -95,6 +172,10 @@ public class RedisPool extends Queued{
 		host = PropertiesConstants.getString(p,"host", "",true);		
 		port = PropertiesConstants.getInt(p, "port", port,true);
 		password = PropertiesConstants.getString(p,"password","",true);
+		username = PropertiesConstants.getString(p,"username",username,true);
+		coder = PropertiesConstants.getString(p,"coder",coder,true);
+		callback = PropertiesConstants.getString(p,"callback",callback);
+		callbackId = PropertiesConstants.getString(p,"callbackId",callbackId);
 		db = PropertiesConstants.getInt(p, "defaultDB", db,true);
 		
 		timeout = PropertiesConstants.getInt(p,"timeout", timeout);
